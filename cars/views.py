27615c5b16a5b97131cc8745ed107d34bafc7eca
VitorMonteiroVianna <vmonteiro2004@gmail.com>
2024-08-01 19:29:34 -0300

    connect models to view

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from cars.models import Cars
 
 
 def cars_view(request):
+    cars = Cars.objects.all()
+    
     return(
         render(
             request,
-            'cars.html'
+            'cars.html',
+            {"cars" : cars}
         )
     )
