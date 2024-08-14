c4f044042efb2308c17d9abc4bbd62fc7574181a
VitorMonteiroVianna <vmonteiro2004@gmail.com>
2024-08-14 17:13:41 -0300

    Add working register form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 
 def register_view(request):
-    user_creation_form = UserCreationForm()
+    if request.method == 'POST':
+        user_creation_form = UserCreationForm(request.POST)
+        if user_creation_form.is_valid():
+            user_creation_form.save()
+            return redirect('cars_list')
+        
+    else:
+        user_creation_form = UserCreationForm()
+    
     return render(request, 'register_user.html', {"user_creation_form": user_creation_form})
