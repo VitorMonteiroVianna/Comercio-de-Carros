e1a1ca66ef1cda0e1c514ea0758d687c8d1c872c
VitorMonteiroVianna <vmonteiro2004@gmail.com>
2024-08-14 16:31:33 -0300

    Add user_registrer django app

--- app/urls.py
+++ app/urls.py
@@ -4,11 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from cars.views import cars_view, add_cars_view
+from accounts.views import register_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars', cars_view, name='cars_list'),
-    path('cars/add', add_cars_view, name='add_car')
+    path('cars/add', add_cars_view, name='add_car'),
+    path('register', register_view, name='register_user')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
