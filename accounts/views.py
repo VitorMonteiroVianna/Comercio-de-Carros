6fcc52cc4f60443953847c34b950fb87393cd11a
VitorMonteiroVianna <vmonteiro2004@gmail.com>
2024-08-19 19:42:13 -0300

    Add authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
 
 
 def register_view(request):
@@ -7,9 +8,26 @@ def register_view(request):
         user_creation_form = UserCreationForm(request.POST)
         if user_creation_form.is_valid():
             user_creation_form.save()
-            return redirect('cars_list')
+            return redirect('authenticate_user')
         
     else:
         user_creation_form = UserCreationForm()
     
     return render(request, 'register_user.html', {"user_creation_form": user_creation_form})
+
+
+def authenticate_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user_in_db = authenticate(request, username=username, password=password)
+        if user_in_db is not None:
+            login(request, user_in_db)
+            return redirect('cars_list')
+        else:
+            user_authentication_form = AuthenticationForm()
+        
+    else:
+        user_authentication_form = AuthenticationForm()
+    
+    return render(request, 'authenticate_user.html', {"user_authentication_form": user_authentication_form})
--- app/urls.py
+++ app/urls.py
@@ -4,13 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from cars.views import cars_view, add_cars_view
-from accounts.views import register_view
+from accounts.views import register_view, authenticate_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars', cars_view, name='cars_list'),
     path('cars/add', add_cars_view, name='add_car'),
-    path('register', register_view, name='register_user')
+    path('accounts/register', register_view, name='register_user'),
+    path('accounts/auth', authenticate_view, name='authenticate_user')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
